a42f6da772450d907e08ae7bf68e927d82fa1af7
Tales Rodrigues Jardim <talesjardim@gmail.com>
2024-06-16 02:16:19 -0300

    #Finaliza desafio- cria conta

--- src/main/java/org/example/ContaTerminal.java
+++ src/main/java/org/example/ContaTerminal.java
@@ -1,17 +1,34 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ContaTerminal {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner terminal = new Scanner(System.in);
+
+        ContaBanco contaBanco = new ContaBanco();
+
+        System.out.println("Criando uma conta de banco");
+        System.out.println("---------------------------");
+        System.out.println("Entre com o nome");
+
+        String name = terminal.nextLine();
+        contaBanco.setNomeCliente(name);
+
+        System.out.println("Entre com o valor do depósito");
+        double deposito = terminal.nextDouble();
+        contaBanco.setSaldo(deposito);
+
+        Random rand = new Random();
+        int numeroContaBanco = rand.nextInt(50);
+        contaBanco.setNumero(numeroContaBanco);
+        contaBanco.setAgencia("001-5");
+
+        System.out.println("Conta criada com sucesso!");
+        System.out.println(contaBanco);
     }
 }
